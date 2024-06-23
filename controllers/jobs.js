32122be833630f6aedc37202bbc07b29fb5cd95f
purvilnakrani <purvilpatel1234@gmail.com>
2024-06-23 17:23:00 +0530

    final

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -3,7 +3,8 @@ const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+  const jobs = await Job.find({ createdBy: req.user.userId })
+                        .sort('createdAt')
   res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 const getJob = async (req, res) => {
